d1f89ba64d641081524c2155eb63c2c7cafcea64
Carlos <c.iglesias.2018@alumnos.urjc.es>
2022-03-02 10:57:27 +0100

    funciona aunque se pierde muchísimo

--- bt_behavior/src/patrolling_main.cpp
+++ bt_behavior/src/patrolling_main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char * argv[])
   factory.registerFromPlugin(loader.getOSName("br2_getwaypoint_bt_node"));
 
   std::string pkgpath = ament_index_cpp::get_package_share_directory("bt_behavior");
-  std::string xml_file = pkgpath + "/behavior_tree_xml/simple2.xml";
+  std::string xml_file = pkgpath + "/behavior_tree_xml/simple.xml";
 
   auto blackboard = BT::Blackboard::create();
   blackboard->set("node", node);
