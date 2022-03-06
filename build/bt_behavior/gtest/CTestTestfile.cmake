55f95ce4041de01ede808d06791866d93dc5e49d
Jhon Delgado <alejandro1jl@gmail.com>
2022-03-06 19:51:28 +0100

    Cleaning files

--- build/bt_behavior/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/foxy/src/gtest_vendor
-# Build directory: /home/jhondl/Documents/universidad/2021/Planificacion/colcon_ws/src/navigation_Roboros/build/bt_behavior/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
